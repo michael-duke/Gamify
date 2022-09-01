66cbce82d06e051259f879a85674c326cb81c20a
michael-duke <michael.21duke@gmail.com>
2022-09-01 12:18:15 +0300

    Add event listener on the nav links
    - Render relevant page on click of the navlinks.
    - Make the website responsive on small- medium screens

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import './style.css';
 import 'tw-elements';
 
 import GameList from './modules/gameList';
+import renderTrailers from './modules/trailers';
+import renderGames from './modules/games';
+import renderContact from './modules/contact';
 
 const gameList = new GameList();
-gameList.loadGames();
 
 // Modal Action
 const modal = document.getElementById('modal');
 modal.onclick = (e) => {
-  const { target: { nodeName } } = e;
+  const {
+    target: { nodeName },
+  } = e;
   if (nodeName === 'ARTICLE') {
     modal.classList.remove('scale-100');
     modal.classList.add('scale-0');
@@ -21,3 +25,47 @@ closebutton.onclick = () => {
   modal.classList.add('scale-0');
   modal.classList.remove('scale-100');
 };
+
+// Nav Action
+const tabLinks = document.querySelectorAll('.nav-link');
+const content = document.getElementById('content');
+
+const renderPage = (location, page) => {
+  location.innerHTML = '';
+  page();
+};
+
+const removeActive = () => {
+  tabLinks.forEach((link) => {
+    if (link.classList.contains('active')) {
+      link.classList.remove('active');
+    }
+  });
+};
+
+tabLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    const { target } = e;
+    if (target.textContent.slice(0, 5) === 'Games') {
+      removeActive();
+      target.classList.add('active');
+      renderPage(content, renderGames);
+      gameList.loadGames();
+    }
+
+    if (target.textContent === 'Trailers') {
+      removeActive();
+      target.classList.add('active');
+      renderPage(content, renderTrailers);
+    }
+
+    if (target.textContent === 'Contact') {
+      removeActive();
+      e.target.classList.add('active');
+      renderPage(content, renderContact);
+    }
+  });
+});
+
+renderPage(content, renderGames);
+gameList.loadGames();
